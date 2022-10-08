ce316ec8843f74b4aac8bb7681ea33eed270af52
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2022-10-08 18:59:39 +0200

    créé le fichier runnable final run.py
    Assure la reproductibilité de nos résultats (c.f.: PDF - Step 4 - Code - Reproducibility).

---
+++ run.py
@@ -0,0 +1,5 @@
+''' PDF - Step 4 - Code - Reproducibility:
+In your submission, you must provide a script run.py 
+which produces exactly the same .csv predictions which you used in your best submission to the competition system.
+If the performance in your submission is worse than the best one on competition system (due to fixing bug, randomness, etc.), 
+then you need to mention the test performance of your submission in the report. '''
